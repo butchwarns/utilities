d2188927d3a05ff02f847dafb3d1d70c5885a9fa
Butch Warns <contact@butchwarns.de>
2023-09-09 20:11:26 +0200

    Add linear parameter smoothign class

---
+++ source/dsp/SmoothLinear.cpp
@@ -0,0 +1,50 @@
+#include "SmoothLinear.h"
+
+SmoothLinear::SmoothLinear()
+    : sample_rate(1.0), num_frames(1.0f), target_val(0.0f), state(0.0f), delta(0.0f)
+{
+}
+
+SmoothLinear::~SmoothLinear()
+{
+}
+
+void SmoothLinear::reset(double _sample_rate)
+{
+    state = 0.0;
+    sample_rate = _sample_rate;
+}
+
+void SmoothLinear::set_time_constant(float time_constant)
+{
+    num_frames = time_constant * sample_rate;
+}
+
+void SmoothLinear::set_target_val(float _target_val)
+{
+    target_val = _target_val;
+    delta = (target_val - state) / num_frames;
+}
+
+float SmoothLinear::next()
+{
+    // Rising
+    if (delta >= 0.0f)
+    {
+        if (state >= target_val)
+        {
+            delta = 0.0f;
+        }
+    }
+    // Falling
+    else
+    {
+        if (state <= 0.0f)
+        {
+            delta = 0.0f;
+        }
+    }
+
+    state += delta;
+    return state;
+}
---
+++ source/dsp/SmoothLinear.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class SmoothLinear
+{
+public:
+    SmoothLinear();
+    ~SmoothLinear();
+
+    void reset(double _sample_rate);
+    void set_time_constant(float time_constant);
+
+    void set_target_val(float _target_val);
+    float next();
+
+private:
+    double sample_rate;
+    float num_frames;
+    float target_val;
+    float state;
+    float delta;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SmoothLinear)
+};
