24a398afe4901fba790b26a432d5e16a960071ce
Butch Warns <contact@butchwarns.de>
2023-09-09 19:09:43 +0200

    Update volume param to work in decibels with mute

--- source/PluginParameters.cpp
+++ source/PluginParameters.cpp
@@ -27,7 +27,28 @@ void PluginParameters::replace_state(juce::ValueTree const &new_state)
 
 float PluginParameters::volume()
 {
-    return *volume_norm;
+    const float db = bdsp::maps::map_linear_norm_pos<float>(*volume_norm, -66.1f, 35.0f);
+    const float gain = bdsp::decibel::db_to_raw_gain_off(db, -66.0f);
+    return gain;
+}
+
+float PluginParameters::normalise_volume(float gain)
+{
+    // Find dB value for given gain
+    float db = 0.0f;
+    if (gain <= bdsp::decibel::db_to_raw_gain(-66.0f))
+    {
+        db = -66.0f;
+    }
+    else
+    {
+        db = bdsp::decibel::raw_gain_to_db(gain);
+    }
+
+    // Normalise
+    const float norm = bdsp::maps::map_linear<float>(db, -66.0, 35.0f, 0.0f, 1.0f);
+
+    return norm;
 }
 
 Apvts::ParameterLayout PluginParameters::parameter_layout()
@@ -37,7 +58,7 @@ Apvts::ParameterLayout PluginParameters::parameter_layout()
     typedef juce::AudioProcessorParameterGroup ParameterGroup;
 
     std::unique_ptr<ParameterGroup> utility_grp = std::make_unique<ParameterGroup>("utility", "UTILITY", "|");
-    utility_grp->addChild(std::make_unique<juce::AudioParameterFloat>("volume", "VOLUME", 0.0f, 1.0f, 0.0f));
+    utility_grp->addChild(std::make_unique<juce::AudioParameterFloat>("volume", "VOLUME", 0.0f, 1.0f, normalise_volume(1.0f)));
 
     layout.add(std::move(utility_grp));
 
