979c8b657b3ebce256667ab0327440aa077b1d5b
Butch Warns <contact@butchwarns.de>
2023-09-10 16:18:04 +0200

    Update named constants header

--- source/no_magic_numbers.h
+++ source/no_magic_numbers.h
@@ -4,4 +4,5 @@
 constexpr int NUM_CHANNELS = 2;
 
 // Value of time constant found by trial and error
-constexpr float SMOOTHING_TIME_CONSTANT = 0.0001f;
+constexpr float SMOOTHING_TIME_DEFAULT = 0.0001f;
+constexpr float SMOOTHING_TIME_WIDTH = 0.00006f;
