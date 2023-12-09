27a2eb8d22fca3aa7ae60623485aea2c3a834fc3
Butch Warns <contact@butchwarns.de>
2023-12-10 00:33:16 +0100

    Comment out parts for speedy testing

--- ci_cd/create_release_mac.sh
+++ ci_cd/create_release_mac.sh
@@ -23,62 +23,62 @@ cd "$ROOT/build"
 cp -R "$ROOT/build/${PLUGIN}_artefacts/Release/AU/$PLUGIN.component" "$ROOT/ci_cd/bin"
 cp -R "$ROOT/build/${PLUGIN}_artefacts/Release/VST3/$PLUGIN.vst3" "$ROOT/ci_cd/bin"
 
-# Run pluginval on the VST3
-echo  "##########################################"
-echo -e "\nValidating VST3 using pluginval..\n"
-cd "$ROOT/ci_cd/bin"
-curl -LO "https://github.com/Tracktion/pluginval/releases/download/v1.0.3/pluginval_macOS.zip"
-7z x pluginval_macOS.zip
-./pluginval.app/Contents/MacOS/pluginval --strictness-level 10 --verbose --validate-in-process "${PLUGIN}.vst3" || exit 1
-echo -e "\nAll tests passed!\n"
-echo  "##########################################"
+# # Run pluginval on the VST3
+# echo  "##########################################"
+# echo -e "\nValidating VST3 using pluginval..\n"
+# cd "$ROOT/ci_cd/bin"
+# curl -LO "https://github.com/Tracktion/pluginval/releases/download/v1.0.3/pluginval_macOS.zip"
+# 7z x pluginval_macOS.zip
+# ./pluginval.app/Contents/MacOS/pluginval --strictness-level 10 --verbose --validate-in-process "${PLUGIN}.vst3" || exit 1
+# echo -e "\nAll tests passed!\n"
+# echo  "##########################################"
 
-# Turn our base64-encoded certificate back to a regular .p12 file
-cd "$ROOT"
-echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+# # Turn our base64-encoded certificate back to a regular .p12 file
+# cd "$ROOT"
+# echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
 
-# We need to create a new keychain, otherwise using the certificate will prompt
-# with a UI dialog asking for the certificate password, which we can't
-# use in a headless CI environment
-security create-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain 
-security default-keychain -s build.keychain
-security unlock-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
-security import certificate.p12 -k build.keychain -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+# # We need to create a new keychain, otherwise using the certificate will prompt
+# # with a UI dialog asking for the certificate password, which we can't
+# # use in a headless CI environment
+# security create-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain 
+# security default-keychain -s build.keychain
+# security unlock-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+# security import certificate.p12 -k build.keychain -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign
+# security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$MACOS_CI_KEYCHAIN_PWD" build.keychain
 
-# Codesign & Notarize
+# # Codesign & Notarize
 
-cd "$ROOT/ci_cd/bin"
+# cd "$ROOT/ci_cd/bin"
 
-echo  "##########################################"
-echo -e "\nCodesign\n"
-/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime $PLUGIN.vst3 -v
-/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime $PLUGIN.component -v
+# echo  "##########################################"
+# echo -e "\nCodesign\n"
+# /usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime $PLUGIN.vst3 -v
+# /usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime $PLUGIN.component -v
 
-# Store the notarization credentials so that we can prevent a UI password dialog
-# from blocking the CI
-echo "Create keychain profile"
-xcrun notarytool store-credentials "notarytool-profile" --apple-id "$PROD_MACOS_NOTARIZATION_APPLE_ID" --team-id "$PROD_MACOS_NOTARIZATION_TEAM_ID" --password "$PROD_MACOS_NOTARIZATION_PWD"
+# # Store the notarization credentials so that we can prevent a UI password dialog
+# # from blocking the CI
+# echo "Create keychain profile"
+# xcrun notarytool store-credentials "notarytool-profile" --apple-id "$PROD_MACOS_NOTARIZATION_APPLE_ID" --team-id "$PROD_MACOS_NOTARIZATION_TEAM_ID" --password "$PROD_MACOS_NOTARIZATION_PWD"
 
-# We can't notarize an app bundle directly, but we need to compress it as an archive.
-# Therefore, we create a zip file containing our app bundle, so that we can send it to the
-# notarization service
-echo "Creating temp notarization archive"
-zip -r ${PLUGIN}_Mac.zip $PLUGIN.vst3 $PLUGIN.component
+# # We can't notarize an app bundle directly, but we need to compress it as an archive.
+# # Therefore, we create a zip file containing our app bundle, so that we can send it to the
+# # notarization service
+# echo "Creating temp notarization archive"
+# zip -r ${PLUGIN}_Mac.zip $PLUGIN.vst3 $PLUGIN.component
 
-# Here we send the notarization request to the Apple's Notarization service, waiting for the result.
-# This typically takes a few seconds inside a CI environment, but it might take more depending on the App
-# characteristics. Visit the Notarization docs for more information and strategies on how to optimize it if
-# you're curious
-echo  "##########################################"
-echo -e "\nNotarize\n"
-xcrun notarytool submit --verbose "${PLUGIN}_Mac.zip" --keychain-profile "notarytool-profile" --wait --timeout 30m
+# # Here we send the notarization request to the Apple's Notarization service, waiting for the result.
+# # This typically takes a few seconds inside a CI environment, but it might take more depending on the App
+# # characteristics. Visit the Notarization docs for more information and strategies on how to optimize it if
+# # you're curious
+# echo  "##########################################"
+# echo -e "\nNotarize\n"
+# xcrun notarytool submit --verbose "${PLUGIN}_Mac.zip" --keychain-profile "notarytool-profile" --wait --timeout 30m
 
-# Finally, we need to "attach the staple" to our executable, which will allow our app to be
-# validated by macOS even when an internet connection is not available.
-echo "Attach staple"
-xcrun stapler staple $PLUGIN.vst3
-xcrun stapler staple $PLUGIN.component
+# # Finally, we need to "attach the staple" to our executable, which will allow our app to be
+# # validated by macOS even when an internet connection is not available.
+# echo "Attach staple"
+# xcrun stapler staple $PLUGIN.vst3
+# xcrun stapler staple $PLUGIN.component
 
 # Create zip archive
 echo  "##########################################"
