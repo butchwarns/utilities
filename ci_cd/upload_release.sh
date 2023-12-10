7db5307f8ae27061407fae72ef957441415eae17
Butch Warns <contact@butchwarns.de>
2023-12-10 01:51:31 +0100

    Update release script

--- ci_cd/upload_release.sh
+++ ci_cd/upload_release.sh
@@ -3,8 +3,10 @@
 # Enable shell debug output
 set -x
 
+PLUGIN="bw_utility"
+
 # Go to project root folder
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 cd "$ROOT/ci_cd/bin"
 
-gh release create "$GITHUB_REF_NAME" -F *.zip
+gh release create "$GITHUB_REF_NAME" -F ../../CHANGELOG.md PLUGIN*.zip
