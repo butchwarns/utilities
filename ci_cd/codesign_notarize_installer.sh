b3fe1f4a4966a0fdaaedcefb2b69e132b828bd59
Butch Warns <contact@butchwarns.de>
2023-12-10 20:34:13 +0100

    Update

--- ci_cd/codesign_notarize_installer.sh
+++ ci_cd/codesign_notarize_installer.sh
@@ -12,7 +12,6 @@ cd "$ROOT/ci_cd/packaging/mac"
 # Turn our base64-encoded certificate back to a regular .p12 file
 echo $MACOS_CERTIFICATE_INSTALLER | base64 --decode > certificate_installer.p12
 
-security create-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain 
 security default-keychain -s build.keychain
 security unlock-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
 security import certificate_installer.p12 -k build.keychain -P "$MACOS_CERTIFICATE_INSTALLER_PWD" -T /usr/bin/productsign
