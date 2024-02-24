4916fb0d075fc8ab855d527d0e7b193c07f0aa37
Butch Warns <contact@butchwarns.de>
2024-02-24 12:22:57 +0100

    Update license texts

--- source/dsp/CrossoverFilter.h
+++ source/dsp/CrossoverFilter.h
@@ -1,6 +1,6 @@
 /*
 CrossoverFilter.h
-Copyright (C) 2023 Butch Warns
+Copyright (C) 2024 Butch Warns
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
